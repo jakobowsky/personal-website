2955a1e8bd587ac9520f7153aceb52799a96709a
Jakob Wolitzki <jakob.wolitzki@gmail.com>
2019-04-15 23:36:40 +0200

    ig posts on template

--- webapp/blog/views.py
+++ webapp/blog/views.py
@@ -2,14 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
+from .models import InstaPost
+
 def get_instagram_posts():
-    pass
+    return InstaPost.objects.all()
 
 
 def home(request):
     template = loader.get_template('blog/index.html')
     context = {
-        'test': '123'
+        'posts': get_instagram_posts()
     }
     return HttpResponse(template.render(context, request))
     
